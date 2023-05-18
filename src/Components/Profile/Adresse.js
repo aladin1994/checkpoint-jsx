9b5c0f567e0c502f3e767728718be1743ac2bc86
aladin1994 <aladinlfig@gmail.com>
2023-05-18 14:06:10 +0100

    jsx

---
+++ src/Components/Profile/Adresse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adresse = () => {
+  return (
+    <div>
+      <p> 19 Rue ibn khaldoun cite ibn rachik Tunis °2001</p>
+    </div>
+  )
+}
+
+export default Adresse
---
+++ src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from '../Profile/image.jfif'
+const ProfilePhoto = () => {
+  return (
+    <div>
+    <img src={image} alt='404'></img>
+    </div>
+  )
+}
+
+export default ProfilePhoto
